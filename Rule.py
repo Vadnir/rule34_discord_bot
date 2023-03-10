c3cd6db66450ef994ce508d8ebd07035ad23a90e
Vadnir <87151072+Vadnir@users.noreply.github.com>
2023-03-10 12:02:48 -0300

    Update Rule.py

--- Rule.py
+++ Rule.py
@@ -8,14 +8,28 @@ class Rule:
 
     async def random_post(self, tags: list[str]) -> str or None:
         if len(tags) < 1:
-            return self.api.random_post().image
+            post = self.api.random_post()
 
-        if len(tags) > 2:
-            try:
-                return self.api.random_post(tags=random.choices(tags, k=2)).image
-            except:
-                return None
-        try:
-            return self.api.random_post(tags=tags).image
-        except:
+        elif len(tags) > 2:
+            post = self.api.random_post(tags=random.choices(tags, k=2))
+
+        else:
+            post = self.api.random_post(tags=tags)
+
+        if isinstance(post, list):
             return None
+        return post.image
+
+    async def search(self, tags: list[str] = (), limit: int = 10, page: int = 1) -> list[str] or None:
+        if len(tags) < 1:
+            posts = self.api.search(tags=tags, limit=limit, page_id=page)
+
+        elif len(tags) > 2:
+            posts = self.api.search(tags=random.choices(tags, k=2), limit=limit, page_id=page)
+        else:
+            posts = self.api.search(tags=tags, limit=limit, page_id=page)
+
+        if len(posts) < 1:
+            return None
+
+        return list(map(lambda x: x.image, posts))
